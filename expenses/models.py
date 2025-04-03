579e05aab0d99e29f21a43b28d30a3ceaef9dfcd
Reshord <daribayev1982@bk.ru>
2025-04-03 14:26:29 +0500

    update

--- expenses/models.py
+++ expenses/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Expense(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.description} - {self.amount}"
+    
--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
-from .models import Expense
+from django.shortcuts import render, redirect
+from .models import Expense, Category
+from .forms import ExpenseForm, DateFilterForm
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 def expense_list(request):
     expenses = Expense.objects.all()
-    return render(request, 'expenses/expense_list.html', {'expenses': expenses})
+    form = DateFilterForm(request.GET)
+    if form.is_valid():
+        start_date = form.cleaned_data.get('start_date')
+        end_date = form.cleaned_data.get('end_date')
+        category = form.cleaned_data.get('category')
+        if start_date:
+            expenses = expenses.filter(date__gte=start_date)
+        if end_date:
+            expenses = expenses.filter(date__lte=end_date)
+        if category:
+            expenses = expenses.filter(category=category)
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses, 'form': form})
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', {'form': form})
+
+def edit_expense(request, pk):
+    expense = Expense.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, 'expenses/edit_expense.html', {'form': form})
+
+def delete_expense(request, pk):
+    expense = get_object_or_404(Expense, pk=pk)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('expense_list')
+    return render(request, 'expenses/delete_expense.html', {'expense': expense})
+
+def add_category(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if name:
+            Category.objects.create(name=name)
+            return redirect('expense_list')
+    return render(request, 'expenses/add_category.html')
